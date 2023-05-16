23bfc43ea5229ff5c76ec24fc9840c72b11ca141
russell258 <duckie258@gmail.com>
2023-05-16 13:36:09 +0800

    dummy

--- src/main/java/sg/nus/iss/visa/ssf/day12demo/controller/DemoController.java
+++ src/main/java/sg/nus/iss/visa/ssf/day12demo/controller/DemoController.java
@@ -3,6 +3,7 @@ package sg.nus.iss.visa.ssf.day12demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +15,9 @@ public class DemoController {
         return new HelloWorld("Hello World");
     }
 
+
     @GetMapping("/home2")
-    public String getHome2(){
+    public String getHome2(@RequestParam String name){
         return "Hello World2";
     }
 
